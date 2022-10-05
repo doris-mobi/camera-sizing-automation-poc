5d9114968d8b910615ad1645b2189fc8b583d70c
Rafael Cavalcante <rafaelc@doris.mobi>
2022-10-05 14:37:04 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Install dependencies
+
+Use `yarn`
+
+## Generate build
+
+Use `yarn build`
---
+++ src/App.js
@@ -0,0 +1,7 @@
+import "./global.css";
+
+function App() {
+  return <h1>Helll</h1>;
+}
+
+export default App;
