7992f56d12c4a6c43c7342d3482200b50870fd60
Rafael Cavalcante <eu@rafaelcavalcante.com>
2022-10-06 16:42:29 -0300

    feat: update timer

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -1,5 +1,5 @@
 import styles from "./styles.module.css";
 
 export const Timer = () => {
-  return <span className={styles.text}>1</span>;
+  return <span className={styles.text}>TIMER IS RUNNING</span>;
 };
